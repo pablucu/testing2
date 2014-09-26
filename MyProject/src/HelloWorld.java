7047f2f66bb4f92acab719a4bc88de71bba76132
rodriguezp <rodriguezp@SD-3PBHQ4J.eu.tsacorp.com>
2014-09-26 08:37:23 +0100

    Hello World Modified

--- MyProject/src/HelloWorld.java
+++ MyProject/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("HELLO WORLD");
+		System.out.print("HELLO WORLD MODIFIED");
 
 	}
 
