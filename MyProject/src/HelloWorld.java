48c08ddbfab8cd01b2e73c099aa118236118307e
Eamonn Shinners-Kennedy <eamonn.shinnerskennedy@aciworldwide.com>
2014-09-26 09:25:40 +0100

    Rene is watching us modification

--- MyProject/src/HelloWorld.java
+++ MyProject/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("HELLO WORLD MODIFIED");
+		System.out.print("HELLO WORLD MODIFIED - Rene is wathcing us");
 
 	}
 
